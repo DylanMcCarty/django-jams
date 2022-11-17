12231cb1ede8235f9a3151750cd9725dfbd7dd3c
DylanMcCarty <dylan.mccarty@student.awesomeinc.org>
2022-11-17 13:26:10 +0000

    seeing if im uo to date

--- myproject/jams/models.py
+++ myproject/jams/models.py
@@ -17,3 +17,12 @@ class Playlists(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return "name: " + self.name + " || User: " + self.user_id.name 
+
+class Songs(models.Model):
+    album_id = models.ForeignKey(to, on_delete)
+    artist_id = models.ForeignKey(to, on_delete)
+    name = models.CharField()
+    length = models.DecimalField()
+    def __str__(self):
+        return "Album: " + self.album_id.name + " || Artist: " + self.artist_id.name + " || Song Name: " + self.name + " || Length: " + self.length
+
